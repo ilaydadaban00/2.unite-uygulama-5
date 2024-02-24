19300013fc74d5d973b92c2ff7bfbfe2b4e02b12
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-02-24 14:31:42 +0300

    Add files via upload

---
+++ 2.unite uygulama 5/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_uygulama_5
+{
+    public partial class Form1 : Form
+    {
+        private Button button1;
+        private TextBox textBox1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "kontrol et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(498, 303);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string kullanıcı_adi;
+            kullanıcı_adi = textBox1.Text;
+            if (kullanıcı_adi == "ilaydadaban00@gmail.com")
+            {
+                MessageBox.Show("kayıtlıdır");
+            }
+            else
+            {
+                MessageBox.Show("kayıtlı değildir");
+            }
+        }
+    }
+}
